8ca1df6e92d631d73801b8bc22f88b46e5f35e43
Julie Thornton <juliet@ksu.edu>
2020-03-25 09:45:12 -0500

    Initial commit

---
+++ Ksu.Cis300.HuffmanTrees/UserInterface.cs
@@ -0,0 +1,43 @@
+﻿/* UserInterface.cs
+ * Author: Rod Howell
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KansasStateUniversity.TreeViewer2;
+using Ksu.Cis300.PriorityQueueLibrary;
+using Ksu.Cis300.ImmutableBinaryTrees;
+using System.IO;
+
+namespace Ksu.Cis300.HuffmanTrees
+{
+    /// <summary>
+    /// A GUI for a program that builds a Huffman tree for a given input file.
+    /// </summary>
+    public partial class UserInterface : Form
+    {
+        /// <summary>
+        /// Constructs the GUI.
+        /// </summary>
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles a Click event on the "Select a File" button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxSelectFile_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
