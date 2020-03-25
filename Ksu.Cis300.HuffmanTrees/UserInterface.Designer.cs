91567cd20e63d827796684d2ffb8d7a249056e47
NickRuffini <njruffini@gmail.com>
2020-03-25 13:38:48 -0500

    Lab 26 Finished

--- Ksu.Cis300.HuffmanTrees/UserInterface.Designer.cs
+++ Ksu.Cis300.HuffmanTrees/UserInterface.Designer.cs
@@ -35,9 +35,10 @@
             // uxSelectFile
             // 
             this.uxSelectFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.uxSelectFile.Location = new System.Drawing.Point(12, 12);
+            this.uxSelectFile.Location = new System.Drawing.Point(24, 23);
+            this.uxSelectFile.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.uxSelectFile.Name = "uxSelectFile";
-            this.uxSelectFile.Size = new System.Drawing.Size(243, 37);
+            this.uxSelectFile.Size = new System.Drawing.Size(486, 71);
             this.uxSelectFile.TabIndex = 1;
             this.uxSelectFile.Text = "Select a File";
             this.uxSelectFile.UseVisualStyleBackColor = true;
@@ -45,12 +46,14 @@
             // 
             // UserInterface
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(267, 61);
+            this.ClientSize = new System.Drawing.Size(534, 117);
             this.Controls.Add(this.uxSelectFile);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "UserInterface";
             this.Text = "Huffman Trees";
+            this.Load += new System.EventHandler(this.UserInterface_Load);
             this.ResumeLayout(false);
 
         }
--- Ksu.Cis300.HuffmanTrees/UserInterface.cs
+++ Ksu.Cis300.HuffmanTrees/UserInterface.cs
@@ -1,5 +1,5 @@
 ﻿/* UserInterface.cs
- * Author: Rod Howell
+ * Author: Nick Ruffini
  */
 using System;
 using System.Collections.Generic;
@@ -37,7 +37,97 @@ namespace Ksu.Cis300.HuffmanTrees
         /// <param name="e"></param>
         private void uxSelectFile_Click(object sender, EventArgs e)
         {
+            if (uxOpenDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    BinaryTreeNode<byte> t = null;
 
+                    // Add code to build the Huffman tree and assign it to t.
+                    long[] array = CountFrequencies(uxOpenDialog.FileName);
+                    MinPriorityQueue<long, BinaryTreeNode<byte>> leaves = BuildLeaves(array);
+                    t = GetHuffmanTree(leaves);
+
+                    new TreeForm(t, 100).Show();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Accident
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UserInterface_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a frequency table from the given file!
+        /// </summary>
+        /// <param name="fileName"> file that the frequency table is being made from </param>
+        /// <returns> returns an array of long values showing the frequency of bytes in the file </returns>
+        private static long[] CountFrequencies(string fileName)
+        {
+            long[] array = new long[256];
+            using (FileStream inFile = File.OpenRead(fileName))
+            {
+                int k;
+                while ((k = inFile.ReadByte()) != -1)
+                {
+                    byte b = (byte)k;
+                    array[b]++;
+                }
+            }
+            return array;
+        }
+
+        /// <summary>
+        /// Creates a bunch of singleton tree nodes from the nonzero values in the array and adds them to
+        /// a Min Priority Queue
+        /// </summary>
+        /// <param name="bytes"> Array of long values being used </param>
+        /// <returns> Min Priority Queue full of singleton tree nodes</returns>
+        private static MinPriorityQueue<long, BinaryTreeNode<byte>> BuildLeaves(long[] bytes)
+        {
+            MinPriorityQueue<long, BinaryTreeNode<byte>> queue = new MinPriorityQueue<long, BinaryTreeNode<byte>>();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (bytes[i] != 0)
+                {
+                    byte b = (byte)i;
+                    BinaryTreeNode<byte> singleton = new BinaryTreeNode<byte>(b, null, null);
+                    queue.Add(bytes[i], singleton);
+                }
+            }
+            return queue;
+        }
+
+        /// <summary>
+        /// Merges all of the singleton trees into a Huffman tree
+        /// </summary>
+        /// <param name="leaves"> Min Priority Queue containing all the singleton trees </param>
+        /// <returns> Returns the resulting Huffman Tree</returns>
+        private static BinaryTreeNode<byte> GetHuffmanTree(MinPriorityQueue<long , BinaryTreeNode<byte>> leaves)
+        {
+            while (leaves.Count > 1)
+            {
+                long p1 = leaves.MinimumPriority;
+ 
+                BinaryTreeNode<byte> firstRemoved = leaves.RemoveMinimumPriority();
+
+                long p2 = leaves.MinimumPriority;
+                BinaryTreeNode<byte> secondRemoved = leaves.RemoveMinimumPriority();
+
+                BinaryTreeNode<byte> newRoot = new BinaryTreeNode<byte>(0, firstRemoved, secondRemoved);
+                leaves.Add(p1 + p2, newRoot);
+            }
+            return leaves.RemoveMinimumPriority();
         }
     }
 }
